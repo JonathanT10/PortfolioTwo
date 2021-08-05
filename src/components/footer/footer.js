a5ea14f9653e4f265d30daddb3669fb48f2b2dc6
JonathanT10 <jonathan_terrell@yahoo.com>
2021-08-05 10:42:20 -0500

    home.jsx added an image as well as some styling.

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import '../css/home.css'
 
 const Footer = () => {
     return(
-        <div>
+        <div className="foot">
             <script src="https://platform.linkedin.com/badges/js/profile.js" async defer type="text/javascript"></script>
             <div class="badge-base LI-profile-badge" data-locale="en_US" data-size="medium" data-theme="dark" data-type="VERTICAL" data-vanity="jonathan-terrell" data-version="v1">
             <a class="badge-base__link LI-simple-link" href="https://www.linkedin.com/in/jonathan-terrell?trk=profile-badge">Jonathan Terrell</a>
--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import '../components/css/home.css'
+import Dwig from '../images/DoWhatIsGreat.jpg'
 
 
 class Home extends Component{
@@ -12,10 +13,13 @@ class Home extends Component{
 
     render() {
         return(
-            <div className="main">
-                <h1>Freelance Developer</h1>
-                <h2>Jonathan Terrell</h2>
-                <p>My name is Jonathan, I am a born and raised Texan, and US Army Veteran. I live just outside Dallas Texas, in a small town named Forney, with my wife and daughter. For the past seven years I have been working in Information Technology. I have had various roles from Telecommunications Engineer, Systems Engineer, and IT Coordinator. Some of these roles have provided the great opportunity of working with international teams, and I have benefited greatly from this experience. Complex problems are where I find, great enthusiasm and focus. In my free time I enjoy woodworking, cooking, and sports.</p>
+            <div className="behind">
+                <div className="main">
+                    <h1>Freelance Developer</h1>
+                    <h2>Jonathan Terrell</h2>
+                    <p>My name is Jonathan, I am a born and raised Texan, and US Army Veteran. I live just outside Dallas Texas, in a small town named Forney, with my wife and daughter. For the past seven years I have been working in Information Technology. I have had various roles from Telecommunications Engineer, Systems Engineer, and IT Coordinator. Some of these roles have provided the great opportunity of working with international teams, and I have benefited greatly from this experience. Complex problems are where I find, great enthusiasm and focus. In my free time I enjoy woodworking, cooking, and sports.</p>
+                </div>
+                <img className="Dwig" src = {Dwig} height="350" width="40%" alt="The American Flag"></img>
             </div>
         )
     }
