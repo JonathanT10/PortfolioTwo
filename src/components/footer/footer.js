70beca75aca6f4a1590fb2b0e8eb2cd318894789
JonathanT10 <jonathan_terrell@yahoo.com>
2021-08-11 20:09:42 -0500

    footer.js fixed the links to socials to open in new tab. project.jsx added some styling to the cards.

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -6,10 +6,10 @@ import Git from '../footer/GitHub-Mark-64px.jpg'
 const Footer = () => {
     return(
         <div className="foot">
-            <a href="https://www.linkedin.com/in/jonathan-terrell">
+            <a href="https://www.linkedin.com/in/jonathan-terrell" target="blank">
          <img className="Linked" src = {LI} height="30" width="35" alt="Linked In Logo"></img>
          </a> 
-         <a href="https://github.com/JonathanT10">
+         <a href="https://github.com/JonathanT10" target="blank">
          <img className="Git" src = {Git} height="30" width="35" alt="GitHub Logo"></img>
          </a> 
         </div>
--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import '../components/css/home.css';
+import Herc from '../images/Hercules2.jpg';
 
 
 
@@ -22,7 +23,7 @@ class Projects extends Component{
                                 <img></img>
                                 <div className="card-body">
                                      <h4>The Founding Marketplace</h4>
-                                     <p>The Founding Marketplace is an e-commerce application, that puts a priority on where products are made. Patrons register and log in, where you are taken to the main product page. Here you can scroll through all the products offered, and see a picture, who made it and where, see a map pinning the location, and add the product to your cart. Patrons can also leave comments/reviews of products.  In the nav menu at the top left of the screen you can logout or go to your cart. In your cartthere is a checkout button that moves products to your past purchases list, which you can view by clicking on Order History.On the home screen users also have the option to go to the Merchant Log in screen where they can register and log in as a Merchant. Merchants are then directed to the Merchant task screen where they can view Merchant Profile, Add Product, submit a request for Highlighted status, and edit their About section. The Merchant profile will have the Merchant's information, and list all that Merchant's products. There is an upload image button for each product where they can upload an image of their product. The Add Product button takes you to the Add Product page, where you can add a product name, description, address made, and price. The Submit Request for Highlighted status button takes you to the Request Highlighted status page where you can submit a request. There is also a input box where the Merchantcan edit their about section.There is also an admin login that can only be accessed with the URL. When logged in with the admin credentials, the admin can see a list of requests that have been submitted by merchants. The amin can approve or deny these requests.</p>
+                                     <p className="projectp">The Founding Marketplace is an e-commerce application, that puts a priority on where products are made. Patrons register and log in, where you are taken to the main product page. Here you can scroll through all the products offered, and see a picture, who made it and where, see a map pinning the location, and add the product to your cart. Patrons can also leave comments/reviews of products.  In the nav menu at the top left of the screen you can logout or go to your cart. In your cartthere is a checkout button that moves products to your past purchases list, which you can view by clicking on Order History.On the home screen users also have the option to go to the Merchant Log in screen where they can register and log in as a Merchant. Merchants are then directed to the Merchant task screen where they can view Merchant Profile, Add Product, submit a request for Highlighted status, and edit their About section. The Merchant profile will have the Merchant's information, and list all that Merchant's products. There is an upload image button for each product where they can upload an image of their product. The Add Product button takes you to the Add Product page, where you can add a product name, description, address made, and price. The Submit Request for Highlighted status button takes you to the Request Highlighted status page where you can submit a request. There is also a input box where the Merchantcan edit their about section.There is also an admin login that can only be accessed with the URL. When logged in with the admin credentials, the admin can see a list of requests that have been submitted by merchants. The amin can approve or deny these requests.</p>
                                      <a href="https://github.com/JonathanT10/theFoundingMarketplacefront" target="blank" className="btn">Go to Repository</a>
                                 </div>
                             </div>
@@ -32,7 +33,7 @@ class Projects extends Component{
                                 <img></img>
                                 <div className="card-body">
                                      <h4>My Trail</h4>
-                                     <p>Description of project</p>
+                                     <p className="projectp">Description of project</p>
                                      <a href="https://github.com/JonathanT10/theFoundingMarketplacefront" target="blank" className="btn">Go to Repository</a>
                                 </div>
                             </div>
@@ -46,17 +47,17 @@ class Projects extends Component{
                                 <img></img>
                                 <div className="card-body">
                                      <h4>Clone Tube</h4>
-                                     <p>Description of project</p>
+                                     <p className="projectp">Description of project</p>
                                      <a href="https://github.com/JonathanT10/theFoundingMarketplacefront" target="blank" className="btn">Go to Repository</a>
                                 </div>
                             </div>
                         </div>
                         <div className="col-sm">
                             <div className="card">
-                                <img></img>
+                                <img className="projectpic" src = {Herc} alt="Screenshot from my Hercules project."></img>
                                 <div className="card-body">
                                      <h4>RPG Hercules Quest</h4>
-                                     <p>Description of project</p>
+                                     <p className="projectp">Hercules Quest is a game with three quests. You battle three monsters with different attacks, and can choose the heros attack.</p>
                                      <a href="https://github.com/JonathanT10/theFoundingMarketplacefront" target="blank" className="btn">Go to Repository</a>
                                 </div>
                             </div>
