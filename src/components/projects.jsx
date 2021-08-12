8140a36b1381f0beff535b8f6a1af7a7ba17c33f
JonathanT10 <jonathan_terrell@yahoo.com>
2021-08-12 11:57:42 -0500

    project.jsx added images to all projects and added description to mytrail.

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import '../components/css/home.css';
 import Herc from '../images/Hercules2.jpg';
+import Clone from '../images/CloneTube.jpg';
+import Mytrail from '../images/MyTrail2.jpg';
+import TFM from '../images/TFM.jpg';
 
 
 
@@ -20,20 +23,20 @@ class Projects extends Component{
                     <div className="row">
                         <div className="col-sm">
                             <div className="card">
-                                <img></img>
+                                <img className="projectpic" src = {TFM} height="250" width="400" alt="Screenshot from my The Founding Marketplace project."></img>
                                 <div className="card-body">
                                      <h4>The Founding Marketplace</h4>
-                                     <p className="projectp">The Founding Marketplace is an e-commerce application, that puts a priority on where products are made. Patrons register and log in, where you are taken to the main product page. Here you can scroll through all the products offered, and see a picture, who made it and where, see a map pinning the location, and add the product to your cart. Patrons can also leave comments/reviews of products.  In the nav menu at the top left of the screen you can logout or go to your cart. In your cartthere is a checkout button that moves products to your past purchases list, which you can view by clicking on Order History.On the home screen users also have the option to go to the Merchant Log in screen where they can register and log in as a Merchant. Merchants are then directed to the Merchant task screen where they can view Merchant Profile, Add Product, submit a request for Highlighted status, and edit their About section. The Merchant profile will have the Merchant's information, and list all that Merchant's products. There is an upload image button for each product where they can upload an image of their product. The Add Product button takes you to the Add Product page, where you can add a product name, description, address made, and price. The Submit Request for Highlighted status button takes you to the Request Highlighted status page where you can submit a request. There is also a input box where the Merchantcan edit their about section.There is also an admin login that can only be accessed with the URL. When logged in with the admin credentials, the admin can see a list of requests that have been submitted by merchants. The amin can approve or deny these requests.</p>
+                                     <p className="projectp">The Founding Marketplace is an e-commerce application, that puts a priority on where products are made. There are Patrons that will log on to by products, and Merchants who can log on to sale products. There are several highlighted statuses available for Merchants, such as Made in America and Veteran owned business. There is also a button in each product description where Patrons can pull up a google map showing exactly where the product was made.</p>
                                      <a href="https://github.com/JonathanT10/theFoundingMarketplacefront" target="blank" className="btn">Go to Repository</a>
                                 </div>
                             </div>
                         </div>
                         <div className="col-sm">
                             <div className="card">
-                                <img></img>
+                                <img className="projectpic" width="400" height="200" src = {Mytrail} alt="Screenshot from my My Trail project."></img>
                                 <div className="card-body">
                                      <h4>My Trail</h4>
-                                     <p className="projectp">Description of project</p>
+                                     <p className="projectp">My Trail is a social media application, where users can share different trails and hikes that they have been on. Users have profiles where they can post about their hikes, location, and rate it. You can upload images for you profile and create an about me section. There is also a friends list on your profile, where you can view all your friends.</p>
                                      <a href="https://github.com/JonathanT10/My_Trail_Backend" target="blank" className="btn">Go to Repository</a>
                                 </div>
                             </div>
@@ -44,7 +47,7 @@ class Projects extends Component{
                     <div className="row">
                         <div className="col-sm">
                             <div className="card">
-                                <img></img>
+                                <img className="projectpic" src = {Clone} width="500" alt="Screenshot from my CloneTube project."></img>
                                 <div className="card-body">
                                      <h4>Clone Tube</h4>
                                      <p className="projectp">Description of project</p>
