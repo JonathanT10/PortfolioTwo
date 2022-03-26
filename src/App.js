4e129b432c7e3d2790542c0928309a031f61b766
JonathanT10 <jonathan_terrell@yahoo.com>
2022-03-26 09:46:46 -0500

    small change in the home page

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import {Switch, Route, BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './store';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Home from '../src/components/home'
+import Home from '../src/components/home';
 import Navbar from './components/navBar/navBar';
 import Projects from './components/projects';
 import Footer from './components/footer/footer';
